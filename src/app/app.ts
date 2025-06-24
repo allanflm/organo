199d4d5c5e0a6a3c968b86b4cd9b84df5ba15f0c
Allan <allanfelipedk@gmail.com>
2025-06-24 14:04:14 -0300

    feat: criando a aplicação

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Cabecalho } from "./components/cabecalho/cabecalho";
+import { Rodape } from "./components/rodape/rodape";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Cabecalho,
+    Rodape],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
